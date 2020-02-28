0589dd50830ce12eb827ba5721d33d67277a058e
sarikavenu <54090691+sarikavenu@users.noreply.github.com>
2020-02-27 23:18:12 -0500

    Update GatewayService.java

--- src/main/java/com/venu/services/micro/GatewayService.java
+++ src/main/java/com/venu/services/micro/GatewayService.java
@@ -3,13 +3,15 @@ package com.venu.services.micro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "*")
 @EnableZuulProxy
 @SpringBootApplication
 public class GatewayService {
 
   public static void main(String[] args) {
-    System.out.println("Gateway Modified Venu1");
+    System.out.println("Gateway Enabled CORS Venu1");
     SpringApplication.run(GatewayService.class, args);
   }
 
